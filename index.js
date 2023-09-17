8a656ecfae771b803ccc322da570c90404f4df62
Luigi <lordwells79@hotmail.it>
2023-09-17 19:30:30 +0200

    try

--- index.js
+++ index.js
@@ -245,7 +245,8 @@ app.get("/", (req, res) => {
   res.send("Hey this is my API running 🥳");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+module.exports = app;
